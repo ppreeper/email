20db75f78779beabe0f891eb92e7a57b53c867f1
Peter Preeper <ppreeper@gmail.com>
2018-03-02 10:19:46 -0700

    add null tests

---
+++ email_test.go
@@ -0,0 +1,19 @@
+package email_test
+
+import "testing"
+
+func BuildMessage(t *testing.T) {
+
+}
+
+func TestAttach(t *testing.T) {
+
+}
+
+func TestServerName(t *testing.T) {
+
+}
+
+func TestSend(t *testing.T) {
+
+}
