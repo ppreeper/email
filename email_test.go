9da9263b8ddc3f647fecc3c6e814a4e4ad46cd63
Peter Preeper <ppreeper@gmail.com>
2018-03-02 10:26:21 -0700

    test ServerName

--- email_test.go
+++ email_test.go
@@ -1,6 +1,10 @@
 package email_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ppreeper/email"
+)
 
 func BuildMessage(t *testing.T) {
 
@@ -11,7 +15,12 @@ func TestAttach(t *testing.T) {
 }
 
 func TestServerName(t *testing.T) {
-
+	host := "test.email.server"
+	port := "25"
+	s := email.SMTPServer{Host: host, Port: port, STARTTLS: false}
+	if s.ServerName() != host+":"+port {
+		t.Errorf("\nSMTPServer.ServerName() failed")
+	}
 }
 
 func TestSend(t *testing.T) {
